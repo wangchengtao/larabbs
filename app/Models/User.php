cc9ce826875cdd9838b0b10a0f840c9ef2a15d88
wangchengtao <915129420@qq.com>
2018-04-21 15:58:36 +0800

    用户可以删除topic

--- app/Models/User.php
+++ app/Models/User.php
@@ -56,4 +56,9 @@ class User extends Authenticatable
     {
         return $this->hasMany(Topic::class);
     }
+
+    public function isAuthorOf($model)
+    {
+        return $this->id == $model->user_id;
+    }
 }
