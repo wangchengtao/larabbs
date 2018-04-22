13872d1a9435fca74fc02e0ca13fa85fd23e25a8
wangchengtao <915129420@qq.com>
2018-04-22 22:34:13 +0800

    用户发表回复

---
+++ resources/views/topics/_reply_box.blade.php
@@ -0,0 +1,14 @@
+@include('common.error')
+
+<div class="reply-box">
+    <form action="{{ route('replies.store') }}" method="POST">
+        <input type="hidden" name="_token" value="{{ csrf_token() }}">
+        <input type="hidden" name="topic_id" value="{{ $topic->id }}">
+
+        <div class="form-group">
+            <textarea name="content" id="content" cols="3" placeholder="分享你的想法" class="form-control"></textarea>
+        </div>
+
+        <button type="submit" class="btn btn-sm btn-primary"><i class="fa fa-share"></i>回复</button>
+    </form>
+</div>
